ac12faa444af6d020845af0f76cdf4c834e9df91
Unitato1 <robertsvihla@gmail.com.github.com>
2023-05-04 21:28:56 +0200

    working as it should

--- src/dom.js
+++ src/dom.js
@@ -6,81 +6,119 @@ function toggle_form(main, form) {
 }
 
 const DOMstuff = () => {
+  const content = document.querySelector("#content");
+  const close = document.querySelector("#close");
+  const close_prjct = document.querySelector(".close");
+  let curr_project = document.querySelector("#today");
+  // let month = document.querySelector("#month");
+  let projects_todos = { today: [], month: [] };
+  let todos = projects_todos["today"];
+  for (let cur in projects_todos) {
+    document.querySelector("#" + cur).addEventListener("click", function () {
+      curr_project = this;
+      todos = projects_todos[this.id];
+      submiting.create_todos_of_project(todos_div, todos_navbar, todos);
+    });
+  }
+
+  const main = document.querySelector("main");
+  const form = document.querySelector(".hidden_form");
+  const todo_form = document.querySelector("#todo_form");
+  const add_form = document.querySelector("#plus_to_do");
+  const submit = document.querySelector("#submit");
+  const todos_div = document.querySelector(".todos");
+  const todos_navbar = document.querySelector(".todos_navbar");
+
+  // form values
   const name = document.querySelector("#name");
   const description = document.querySelector("#description");
   const date = document.querySelector("#date");
+  let priority;
   const priority_radio = Array.from(
     document.querySelectorAll("[name='priority']")
   );
 
-  const content = document.querySelector("#content");
-  const close = document.querySelector("#close");
-  const close_prjct = document.querySelector(".close");
+  // add project
+  const add_project_form = document.querySelector("#add_project_form");
+  const add_project = document.querySelector("#add_project");
+  const plus = document.querySelector("#plus");
+  const add_project_btn = document.querySelector("#add_project_btn");
+  let projects = ["today", "month"];
+  const project_name = document.querySelector("#name_project");
+  const nav_bar = document.querySelector(".projects_navbar");
 
-  close.addEventListener("click", (e) => {
-    e.preventDefault();
-    toggle_form(main, form);
-  });
-
-  const main = document.querySelector("main");
-  const add_form = document.querySelector("#plus_to_do");
-  const form = document.querySelector(".hidden_form");
-  const todo_form = document.querySelector("#todo_form");
-  add_form.addEventListener("click", () => {
-    if (add_project.className === "form" || form.className === "form") {
-      return;
+  // submit project + todo
+  add_project_btn.addEventListener("click", (e) => {
+    if (add_project_form.checkValidity()) {
+      e.preventDefault();
+      for (let project of projects) {
+        if (project === project_name.value) {
+          alert("Name Already Taken, Try diffrent one");
+          return;
+        }
+      }
+      let project = submiting.sumbit_project(project_name.value);
+      projects_todos[project.id] = [];
+      project.addEventListener("click", function () {
+        curr_project = this;
+        todos = projects_todos[this.id];
+        submiting.create_todos_of_project(todos_div, todos_navbar, todos);
+      });
+      nav_bar.appendChild(project);
+      projects.push(project_name.value);
+      project_name.value = "";
+      toggle_form(main, add_project);
     }
-    toggle_form(main, form);
   });
-
-  let priority;
-  const submit = document.querySelector("#submit");
-  const todos = [];
-  const todos_div = document.querySelector(".todos");
   submit.addEventListener("click", (e) => {
     if (todo_form.checkValidity()) {
       e.preventDefault();
+      for (let todo of projects_todos[curr_project.id]) {
+        if (todo.name === name.value) {
+          alert("Name Already Taken, Try diffrent one");
+          return;
+        }
+      }
       priority = priority_radio.find((element) => {
         return element.checked;
       });
       todos.push(
         todo(name.value, description.value, date.value, priority.value)
       );
-      todos_div.replaceChildren();
-      for (let curr_todo of todos) {
-        todos_div.appendChild(submiting.submit_todo(curr_todo));
-      }
+      submiting.create_todos_of_project(todos_div, todos_navbar, todos);
+      // todos_div.replaceChildren();
+      // todos_navbar.replaceChildren();
+      // for (let curr_todo of todos) {
+      //   todos_navbar.appendChild(submiting.create_navbar_todos(curr_todo));
+      //   todos_div.appendChild(submiting.submit_todo(curr_todo));
+      // }
       name.value = "";
       description.value = "";
       date.value = "";
-      priority.value = "";
       toggle_form(main, form);
     }
   });
-  const add_project = document.querySelector("#add_project");
-  const plus = document.querySelector("#plus");
-  const add_project_btn = document.querySelector("#add_project_btn");
-  close_prjct.addEventListener("click", (e) => {
-    e.preventDefault();
-    toggle_form(main, add_project);
-  });
-  const projects = [];
-  const project_name = document.querySelector("#name_project");
-  const nav_bar = document.querySelector(".projects_navbar");
 
-  add_project_btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let project = submiting.sumbit_project(project_name.value);
-    nav_bar.appendChild(project);
-    projects.push(project_name.value);
-
-    project_name.value = "";
-    toggle_form(main, add_project);
+  // show forms btns
+  add_form.addEventListener("click", () => {
+    if (add_project.className === "form" || form.className === "form") {
+      return;
+    }
+    toggle_form(main, form);
   });
   plus.addEventListener("click", (e) => {
     if (form.className === "form" || add_project.className === "form") {
       return;
     }
+    toggle_form(main, add_project);
+  });
+  //close btns
+  close.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggle_form(main, form);
+  });
+  close_prjct.addEventListener("click", (e) => {
+    e.preventDefault();
     toggle_form(main, add_project);
   });
 };
--- src/form.js
+++ src/form.js
@@ -13,18 +13,40 @@ const submiting = (() => {
     date_div.textContent = todo.date;
     priority_div.textContent = todo.priority;
     priority_div.className = todo.priority;
-    description_div.textContent = "Description";
+    description_div.textContent = todo.description;
     curr_to_do.append(name_div, date_div, priority_div, description_div);
     curr_to_do.className = "todo";
     return curr_to_do;
   };
+
   const sumbit_project = (name) => {
     const name_project_btn = document.createElement("button");
     name_project_btn.textContent = name;
     name_project_btn.className = "nav_bar_project";
+    name_project_btn.id = name;
     return name_project_btn;
   };
-  return { submit_todo, sumbit_project };
+  const create_navbar_todos = (todo) => {
+    const curr = document.createElement("div");
+    curr.className = "nav_bar_do";
+    curr.textContent = todo.name;
+    return curr;
+  };
+  const create_todos_of_project = (todos_div, todos_navbar, todos) => {
+    todos_div.replaceChildren();
+    todos_navbar.replaceChildren();
+    for (let curr_todo of todos) {
+      todos_navbar.appendChild(create_navbar_todos(curr_todo));
+      todos_div.appendChild(submit_todo(curr_todo));
+    }
+  };
+
+  return {
+    submit_todo,
+    sumbit_project,
+    create_navbar_todos,
+    create_todos_of_project,
+  };
 })();
 
 export { submiting, todo };
